04a9eb76a41859c84fe328cdb4f9b44213c87aaf
pradnyahaval <56785606+pradnyahaval@users.noreply.github.com>
2021-02-16 01:05:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*var audio = document.createElement('audio');
+audio.setAttributes = ('src', 'sound.mp3');
+audio.play();
+audio.loop=true;*/
+/*
+
+var music = new Audio() ;
+music.src = "sound.mp3" ;
+music.play();*/
+
+var snd = new Audio("sound.mp3"); // buffers automatically when created
+snd.play();
